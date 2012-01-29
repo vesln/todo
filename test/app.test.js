aa1c31405fc7a679fc05d3ee5c9d812cb7fa225c
Veselin Todorov <veselin@veselin.bg>
2012-01-29 14:38:31 +0200

    Swaping app and cli.

---
+++ test/app.test.js
@@ -0,0 +1,24 @@
+/*!
+ * todo - Todos in the CLI like what.
+ * 
+ * Veselin Todorov <hi@vesln.com>
+ * MIT License.
+ */
+
+/**
+ * Test dependencies.
+ */
+var flatiron = require('flatiron');
+
+/**
+ * The tests object.
+ * 
+ * @type {Object}
+ */
+var app = require('../lib/app');
+
+describe('app', function() {
+  it('should be flatiron app', function() {
+    app.should.eql(flatiron.app);
+  });
+});
