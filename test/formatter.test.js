5c44ca8f50d35bd675ded977f7b41b2775af789e
Veselin Todorov <veselin@veselin.bg>
2012-01-29 14:52:55 +0200

    Using colors instead of color. + tests

---
+++ test/formatter.test.js
@@ -0,0 +1,25 @@
+/*!
+ * todo - Todos in the CLI like what.
+ * 
+ * Veselin Todorov <hi@vesln.com>
+ * MIT License.
+ */
+
+/**
+ * The tests object.
+ * 
+ * @type {Object}
+ */
+var formatter = require('../lib/formatter');
+
+describe('formatter', function() {
+  describe('.format()', function() {
+    it('should format done items properly', function() {
+      formatter.format({ text: 'Foo', done: true }, 1).should.eql('     #1  \u001b[32m√\u001b[39m  Foo');
+    });
+    
+    it('should format unfinished items properly', function() {
+      formatter.format({ text: 'Foo', done: false }, 1).should.eql('     #1  \u001b[31m✖\u001b[39m  Foo');
+    });
+  });
+});
