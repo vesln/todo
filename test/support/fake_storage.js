048b5bb6c2c8992f0486fa51debf6bab57157b53
Radoslav Stankov <rstankov@gmail.com>
2012-02-05 17:00:40 +0200

    Extract the todos logic into todos object

---
+++ test/support/fake_storage.js
@@ -0,0 +1,25 @@
+var Storage = module.exports = function() {};
+
+Storage.prototype.get = function(type, callback) {
+  if (type != 'items') {
+    throw 'Invalid type - ' + type + ' (should be items)';
+  }
+
+  callback(null, this.items);
+};
+
+Storage.prototype.set = function(type, items, callback) {
+  if (type != 'items') {
+    throw 'Invalid type - ' + type + ' (should be items)';
+  }
+
+  this._items = items;
+
+  callback(null);
+};
+
+Storage.prototype.save = function(callback) {
+  this.items = this._items;
+  delete this._items;
+  callback(null);
+};
