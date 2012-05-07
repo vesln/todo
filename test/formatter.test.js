1a0a85bfe8a53976bd413ff26ba3a08c6ca8671f
Veselin Todorov <veselin@veselin.bg>
2012-05-07 09:21:06 +0300

    Whitespaces

--- test/formatter.test.js
+++ test/formatter.test.js
@@ -1,13 +1,13 @@
 /*!
  * todo - Todos in the CLI like what.
- * 
+ *
  * Veselin Todorov <hi@vesln.com>
  * MIT License.
  */
 
 /**
  * The tests object.
- * 
+ *
  * @type {Object}
  */
 var formatter = require('../lib/formatter');
@@ -17,7 +17,7 @@ describe('formatter', function() {
     it('should format done items properly', function() {
       formatter.format({ text: 'Foo', done: true }, 0).should.eql('     #1  \u001b[32m√\u001b[39m  Foo');
     });
-    
+
     it('should format unfinished items properly', function() {
       formatter.format({ text: 'Foo', done: false }, 0).should.eql('     #1  \u001b[31m✖\u001b[39m  Foo');
     });
