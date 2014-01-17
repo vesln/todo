0f2de01e933218372226911d7b9e4dd1a79d635a
Veselin Todorov <veselin@veselin.bg>
2014-01-17 12:17:33 +0200

    List todo items with given tag

--- test/acceptance/ls.js
+++ test/acceptance/ls.js
@@ -1,9 +1,21 @@
 describe('todo ls', function() {
   it('lists added todo items', function(done) {
     cli()
+    .unlink('/tmp/todos.txt')
     .exec('./todo add have more fun')
     .run('ls')
     .stdout(/have more fun/)
+    .code(0)
+    .end(done);
+  });
+
+  it('can list todo items with given tag', function(done) {
+    cli()
+    .unlink('/tmp/todos.txt')
+    .exec('./todo add have more fun @tag')
+    .exec('./todo add another one')
+    .run('ls @tag')
+    .stdout('have more fun @tag')
     .code(0)
     .end(done);
   });
