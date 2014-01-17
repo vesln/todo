99b39c9b531f31a301f7ba5f798bce4eb4bc5463
Veselin Todorov <veselin@veselin.bg>
2014-01-17 13:15:50 +0200

    `rm` errors when invalid id is provided

--- test/acceptance/rm.js
+++ test/acceptance/rm.js
@@ -9,4 +9,13 @@ describe('todo rm', function() {
     .code(0)
     .end(done);
   });
+
+  it('errors when the supplied id is invalid', function(done) {
+    cli()
+    .exec('./todo add have less fun')
+    .run('rm 2')
+    .stderr('todo: Cannot find a todo item with id "2"')
+    .code(1)
+    .end(done);
+  });
 });
