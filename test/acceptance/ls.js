cab9f8777e97efdb686cb8e93190e261416bbb28
Veselin Todorov <veselin@veselin.bg>
2014-01-17 14:33:55 +0200

    Organize `ls` tests

--- test/acceptance/ls.js
+++ test/acceptance/ls.js
@@ -8,64 +8,63 @@ describe('todo ls', function() {
     .end(done);
   });
 
-  it('can list todo items that contain given string', function(done) {
-    cli()
-    .exec('./todo add have more fun @tag')
-    .exec('./todo add another one')
-    .run('ls another')
-    .stdout('2. another one')
-    .code(0)
-    .end(done);
+  describe('keyword1 keyword2', function() {
+    it('lists the items that contain the given keyword(s)', function(done) {
+      cli()
+      .exec('./todo add have more fun @tag')
+      .exec('./todo add another one')
+      .run('ls another')
+      .stdout('2. another one')
+      .code(0)
+      .end(done);
+    });
   });
 
-  it('can list todo items not containing given string', function(done) {
-    cli()
-    .exec('./todo add have more fun @tag')
-    .exec('./todo add another one')
-    .run('ls ~another')
-    .stdout('1. have more fun @tag')
-    .code(0)
-    .end(done);
+  describe('~keyword1 ~keyword2', function() {
+    it('lists the items that do not contain the given keyword(s)', function(done) {
+      cli()
+      .exec('./todo add have more fun @tag')
+      .exec('./todo add another one')
+      .run('ls ~another')
+      .stdout('1. have more fun @tag')
+      .code(0)
+      .end(done);
+    });
   });
 
-  it('can list todo items not containing one string and containing another', function(done) {
-    cli()
-    .exec('./todo add have more fun @tag')
-    .exec('./todo add have more fun')
-    .exec('./todo add another one')
-    .run('ls ~another @tag')
-    .stdout('1. have more fun @tag')
-    .code(0)
-    .end(done);
+  describe('~keyword1 keyword2', function() {
+    it('does not list items containing one keyword but not containing other', function(done) {
+      cli()
+      .exec('./todo add have more fun @tag')
+      .run('ls ~@tag @tag')
+      .stdout('')
+      .code(0)
+      .end(done);
+    });
   });
 
-  it('does not return todo items when both included and excluded', function(done) {
-    cli()
-    .exec('./todo add have more fun @tag')
-    .run('ls ~@tag @tag')
-    .stdout('')
-    .code(0)
-    .end(done);
+  describe('--done', function() {
+    it('lists completed todos', function(done) {
+      cli()
+      .exec('./todo add have more fun')
+      .exec('./todo check 1')
+      .run('ls --done')
+      .stdout('1. have more fun')
+      .code(0)
+      .end(done);
+    });
   });
 
-  it('can list completed todo items', function(done) {
-    cli()
-    .exec('./todo add have more fun')
-    .exec('./todo check 1')
-    .run('ls --done')
-    .stdout('1. have more fun')
-    .code(0)
-    .end(done);
-  });
-
-  it('can list all todo items', function(done) {
-    cli()
-    .exec('./todo add have more fun')
-    .exec('./todo add another one')
-    .exec('./todo check 1')
-    .run('ls --all')
-    .stdout('1. have more fun\n2. another one')
-    .code(0)
-    .end(done);
+  describe('--all', function() {
+    it('lists all todo items', function(done) {
+      cli()
+      .exec('./todo add have more fun')
+      .exec('./todo add another one')
+      .exec('./todo check 1')
+      .run('ls --all')
+      .stdout('1. have more fun\n2. another one')
+      .code(0)
+      .end(done);
+    });
   });
 });
