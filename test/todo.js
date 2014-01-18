bd4a39278538f864ffec2a437bd07ce76747fb5b
Veselin Todorov <veselin@veselin.bg>
2014-01-18 11:32:23 +0200

    Add unit tests for Todo

---
+++ test/todo.js
@@ -0,0 +1,55 @@
+var Todo = require('../lib/todo');
+
+describe(Todo, function() {
+  it('requires an id', function() {
+    should.throw(function() {
+      new Todo(null, 'foo');
+    });
+  });
+
+  it('requires a description', function() {
+    should.throw(function() {
+      new Todo('id');
+    });
+  });
+
+  it('has a default pending status', function() {
+    var todo = new Todo(1, 'test');
+    todo.status.should.eq('pending')
+  });
+
+  describe('.create', function() {
+    it('returns an array of todo items', function() {
+      var attrs = [
+        { id: 1, desc: 'Buy beer', status: 'done' },
+        { id: 2, desc: 'Feed the pandas', status: 'pending' },
+      ];
+
+      var todos = Todo.create(attrs);
+
+      todos[0].id.should.eq(1);
+      todos[0].desc.should.eq('Buy beer');
+      todos[0].status.should.eq('done');
+      todos[1].id.should.eq(2);
+      todos[1].desc.should.eq('Feed the pandas');
+      todos[1].status.should.eq('pending');
+    });
+  });
+
+  describe('#complete', function() {
+    it('completes the todo item', function() {
+      var todo = new Todo(1, 'test');
+      todo.complete();
+      todo.status.should.eq('done')
+    });
+  });
+
+  describe('#undo', function() {
+    it('undoes the todo', function() {
+      var todo = new Todo(1, 'test');
+      todo.complete();
+      todo.undo();
+      todo.status.should.eq('pending')
+    });
+  });
+});
